7eb9a07553e4597e6e80f9cdda61baaad5490e68
Reece Peardon <peardon.reece@gmail.com>
2024-09-08 14:05:19 -0700

    Bugs quashed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />}></Route>
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
