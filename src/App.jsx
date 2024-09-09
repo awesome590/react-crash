ceb0885cc4c34d53cea5d15c301c388ee77231a4
Reece Peardon <peardon.reece@gmail.com>
2024-09-08 18:37:38 -0700

    did more work

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Nav from './components/Nav.jsx'
+import Users from './pages/Users.jsx'
 
 function App() {
   return (
     <>
       <Router>
+        <Nav></Nav>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />}></Route>
+          <Route path='/users/:username' element={<Users />}></Route>
         </Routes>
       </Router>
     </>
